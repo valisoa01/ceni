9a31f6d16e2fd5838d5461a4a0a9cbfb8a04b786
valisoa01 <valisoatolotriniaina@gmail.com>
2026-02-19 16:03:33 +0300

    first commit

---
+++ src/main/java/com/ceni/CeniApplication.java
@@ -0,0 +1,13 @@
+package com.ceni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ceni/CeniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
